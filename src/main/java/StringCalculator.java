8793bf36127b30edaf2e3be905687128dbda9aef
hutton85is <hutton85is@yahoo.com>
2016-11-08 20:59:02 +0000

    calculator can now return for one number

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -2,4 +2,17 @@ import java.util.*;
 
 public class StringCalculator {
 
+	private boolean isItEmpty(String str) {
+		return str == "";
+	}
+
+	public int sum(String str) {
+		
+		if (isItEmpty(str)) {
+			return 0;
+		}
+		
+		return Integer.parseInt(str);
+	}
+
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -4,9 +4,20 @@ import static org.hamcrest.CoreMatchers.instanceOf;
 
 public class StringCalculatorTest {
 
+	private static StringCalculator CalcTest = new StringCalculator();
+
 	@Test
 	public void ClassTest() {
-		StringCalculator CalcTest = new StringCalculator();
 		assertThat(CalcTest, instanceOf(StringCalculator.class));
+	}
+
+	@Test
+	public void zeroNumberInput() {
+		assertEquals(0, CalcTest.sum(""));
+	}
+
+	@Test
+	public void oneNumberInput() {
+		assertEquals(5, CalcTest.sum("5"));
 	}
 }
